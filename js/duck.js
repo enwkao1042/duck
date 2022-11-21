e1861f1ace731daa8e130290be91ff5b823567e3
enwkao1042 <117055940+enwkao1042@users.noreply.github.com>
2022-11-21 11:49:36 +0900

    Add files via upload

---
+++ js/duck.js
@@ -0,0 +1,16 @@
+$(document).ready(function(){
+	
+	//index페이지에서 페이스 슬라이드
+	const slide = setInterval( fade, 2300);
+	let i=3;
+	function fade(){
+		i--;
+		if(i==0) {
+			$("#index img").stop().fadeIn(1000);
+			i=3;
+		}else {
+			$("#index img").eq(i).stop().fadeOut(1000);
+		}
+	}////fade end
+	
+});///////////////all end
